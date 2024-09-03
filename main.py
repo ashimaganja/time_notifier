92c2538bf1a37e4309121ab6d659477a0350797a
Ashim Aganja <97860757+ashimaganja@users.noreply.github.com>
2024-09-03 18:04:38 +0000

    Add few new features

--- main.py
+++ main.py
@@ -4,4 +4,13 @@ app = FastAPI()
 
 @app.get("/")
 async def index():
-    return {"The timer app": "Version 1.0.1"}  
+    return "This is the homepage"
+
+@app.get("/tasks")
+async def task_list():
+    return "This is the task list" 
+
+@app.get("/new-task")
+async def create_newtask():
+    return "This is where you will create tasks"
+
